9695d2fb4528414623663ae17d875c38dda7c3f3
Irfan Basyaruddin <irfanbasyar.ib@gmail.com>
2022-01-06 21:57:11 +0700

    modulasi dengan package

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Aplikasi deteksi gempa terkini
+MODULARISASI DENGAN FUNCTION
+MODULARISASI DENGAN PACKAGE
+"""
+import gempaterkini
+
+if __name__ == '__main__':
+    print('Aplikasi utama')
+    result = gempaterkini.ekstraksi_data()
+    gempaterkini.tampilkan_hasil(result)
